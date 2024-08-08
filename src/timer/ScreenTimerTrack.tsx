b2aa46f602f3465f196b8a2bae20fac6b8c9607c
matheusperezz <matheus.mperez08@gmail.com>
2024-08-08 12:33:11 -0300

    Mantendo tempo na tela

--- src/timer/ScreenTimerTrack.tsx
+++ src/timer/ScreenTimerTrack.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 
 export const ScreenTimerTrack: React.FC = () => {
     const [timeOnScreen, setTimeOnScreen] = useState<number>(0);
+    const [currentTotalTime, setCurrentTotalTime] = useState<number>(0);
     const [idleTime, setIdleTime] = useState<number>(0);
     const [isIdle, setIsIdle] = useState<boolean>(false);
     const idleThreshold = 15;
@@ -10,39 +11,33 @@ export const ScreenTimerTrack: React.FC = () => {
     const resetIdleTime = useCallback(() => {
         setTotalIdleTime((prevTotalIdleTime) => prevTotalIdleTime + idleTime);
         setIdleTime(0);
+        setCurrentTotalTime(0);
         setIsIdle(false);
-        setTimeOnScreen(0);
     }, [idleTime]);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setTimeOnScreen((prevTime) => {
-                if (prevTime < idleThreshold) {
-                    return prevTime + 1;
-                } else {
-                    setIdleTime((prevIdleTime) => {
-                        const newIdleTime = prevIdleTime + 500;
-                        if (newIdleTime >= idleThreshold) {
-                            setIsIdle(true);
-                        }
-                        return newIdleTime;
-                    });
-                    return prevTime;
-                }
-            });
+            setCurrentTotalTime((prevTime) => prevTime + 1);
+
+            if (currentTotalTime >= idleThreshold) {
+                setIdleTime((prevIdleTime) => prevIdleTime + 1);
+                setIsIdle(true);
+            } else {
+                setTimeOnScreen((prev) => prev + 1);
+            }
         }, 1000);
 
         return () => {
             clearInterval(intervalId);
-        }
-    }, [resetIdleTime]);
+        };
+    }, [currentTotalTime, idleThreshold]);
 
     return (
         <div>
             <div>
                 <p>Tempo na tela: {timeOnScreen} segundos</p>
-                <p>Tempo de ociosidade: {idleTime / 1000} segundos</p>
-                <p>Tempo total de ociosidade: {totalIdleTime / 1000} segundos</p>
+                <p>Tempo de ociosidade: {idleTime} segundos</p>
+                <p>Tempo total de ociosidade: {totalIdleTime} segundos</p>
                 <button onClick={resetIdleTime}>Resetar tempo de ociosidade</button>
                 {isIdle && <p>Usuário está ocioso</p>}
             </div>
